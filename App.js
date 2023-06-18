ea1e4179385a22bb3b401c18d99d5e0ef2f47ae2
diegogirao <diegoramiro25@gmail.com>
2023-06-18 17:25:33 +0100

    Feature: created an example with reduxjs toolkit

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native"
+import { Provider, useSelector, useDispatch } from "react-redux"
+import { configureStore, createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+	counter: 0,
+}
+
+const counterSlice = createSlice({
+	name: "counter",
+	initialState,
+	reducers: {
+		increment: (state) => {
+			state.counter += 1
+		},
+		decrement: (state) => {
+			state.counter -= 1
+		},
+	},
+})
+
+const store = configureStore({
+	reducer: counterSlice.reducer,
+})
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<Provider store={store}>
+			<Counter />
+		</Provider>
+	)
+}
+
+function Counter() {
+	const counter = useSelector((state) => state.counter)
+	const dispatch = useDispatch()
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Counter: {counter}</Text>
+			<View style={styles.button}>
+				<Button
+					title="Increment +"
+					color="#f6f6"
+					onPress={() => dispatch(counterSlice.actions.increment())}
+				/>
+				<Button
+					title="Decrement -"
+					color="#f6f6"
+					onPress={() => dispatch(counterSlice.actions.decrement())}
+				/>
+			</View>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: "#000",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	text: {
+		fontSize: 40,
+		fontWeight: "bold",
+		color: "#38bdf8",
+		marginBottom: 50,
+	},
+	button: {
+		flexDirection: "row",
+		justifyContent: "space-around",
+		alignItems: "center",
+		gap: 30,
+	},
+})
